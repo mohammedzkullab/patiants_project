e423db9ae10fe1d8734eb8eacb7fbcf83cf9f1ac
mohammed kullab <moh.kull200@gmail.com>
2024-07-28 16:59:09 +0300

    working sign in page

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import Providers from "@/lib/Providers";
+import { ThemeProvider } from "next-themes";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "CarePulse",
+  description:
+    "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
+  icons: {
+    icon: "/assets/icons/logo-icon.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" forcedTheme="dark" enableColorScheme>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Forms/index.ts
@@ -0,0 +1,1 @@
+export { PatientForm } from "./PatientForm";
