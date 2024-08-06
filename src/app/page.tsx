04347c8d5930d15fe62393ecc59c1019b5c5f566
mohammed kullab <moh.kull200@gmail.com>
2024-08-06 18:09:17 +0300

    created admin passkey modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { PatientForm } from "@/components/Forms/PatientForm";
-// import { PasskeyModal } from "@/components/PasskeyModal";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
 const Home = ({ searchParams }: SearchParamProps) => {
   const isAdmin = searchParams?.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
-      {/* {isAdmin && <PasskeyModal />} */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
