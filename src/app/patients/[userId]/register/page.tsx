536b03ab172e047f4c94baa0623681e619319a58
mohammed kullab <moh.kull200@gmail.com>
2024-08-09 18:36:55 +0300

    configured sentry and sentry metrics

--- src/app/patients/[userId]/register/page.tsx
+++ src/app/patients/[userId]/register/page.tsx
@@ -3,9 +3,13 @@ import { getPatient, getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
+import * as Sentry from "@sentry/nextjs";
+
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
   const patient = await getPatient(userId);
+
+  Sentry.metrics.set("user_view_register", user.name);
 
   if (patient) redirect(`/patients/${userId}/new-appointment`);
 
